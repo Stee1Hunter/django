1ecfd2150c5b6eceaa061218d7c157c886e18e09
Stee1Hunter <144276556+Stee1Hunter@users.noreply.github.com>
2025-06-20 21:52:45 +0300

    Update settings.py

--- webproj/settings.py
+++ webproj/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pgbqlvtm_fzi!l@#@diag2y7+3c(c(#ywd$is@k^s5e84&)l9k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
